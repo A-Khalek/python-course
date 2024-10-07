70020ae9baae9caa95465f3e56ec43486e0c3724
A-Khalek <khalek.raju.bd@gmail.com>
2024-10-08 02:08:26 +0600

    python course initialize

---
+++ pythonProject/syntex.py
@@ -0,0 +1,15 @@
+# Execute Python Syntax
+
+print("hello world")
+
+# if condition
+
+firstNumber = 5
+secondNumber = 6
+
+if firstNumber>secondNumber:
+    print(str(firstNumber) + " is grater than " + str(secondNumber)) # number and string concat
+else:
+    print(str(firstNumber) + " is less than two " + str(secondNumber))
+
+# output 5 is less than two 6
