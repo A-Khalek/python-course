c1ad96e67dc7ec7edada78d5b853b44c3511b0c0
A-Khalek <khalek.raju.bd@gmail.com>
2024-10-11 22:47:03 +0600

    variables

--- pythonProject/variables/variables.py
+++ pythonProject/variables/variables.py
@@ -35,9 +35,147 @@ else:
 fruits  = ["Apple" , "Orange", "Mango"]
 
 firstFruit , secondFruit , thirdFruit = fruits
-print("Second Fruit is " + secondFruit) # output: Second Fruit is Orange
-print("First Fruit is " + firstFruit) # output: First Fruit is Apple
-print("Third Fruit is " + thirdFruit) # output: Third Fruit is Mango
+# print("Second Fruit is " + secondFruit) # output: Second Fruit is Orange
+# print("First Fruit is " + firstFruit) # output: First Fruit is Apple
+# print("Third Fruit is " + thirdFruit) # output: Third Fruit is Mango
+#
+
+# Bitwise Operations
 
 
-sdfsdf
+# Bitwise AND:
+
+b = 20
+a = 10
+
+result = a & b
+print("Bitwise AND : " + str(result))
+
+# Bitwise OR:
+
+result = a | b
+print("Bitwise OR: "+ str(result))
+
+# যোগ (Addition):
+
+img_a = 3+4j
+img_b = 1-2j
+
+print(img_a + img_b)
+
+
+# List
+mixed_list = [25,"hello",3.25,True]
+print(mixed_list)
+
+
+# List-এর গুরুত্বপূর্ণ অপারেশনসমূহ:
+
+# 1. Index
+print(mixed_list[0])
+print(mixed_list[1])
+
+# 2. Negative Indexing:
+print(mixed_list[-2])
+print(mixed_list[-1])
+
+# 3. Slicing:
+my_list = [10, 20, 30, 40, 50]
+print(my_list[1:3])
+print(my_list[:3])
+print(my_list[2:])
+
+# 4. আইটেম পরিবর্তন (Item Modification):
+print(my_list)
+my_list[0] = 50
+print(my_list)
+
+# 5. আইটেম যোগ করা (Adding Items):
+
+my_list.append(100)
+print(my_list)
+
+# insert() মেথড ব্যবহার করে নির্দিষ্ট index-এ নতুন আইটেম যোগ করা হয়।
+
+my_list.insert(2,500)
+print(my_list)
+
+# 6. আইটেম মুছা (Removing Items):
+# 6.1 remove()
+my_list1 = [1, 2, 3, 4]
+my_list1.remove(3)  # 3 মুছা হলো
+print(my_list1)  # Output: [1, 2, 4]
+
+
+# 6.2 pop() মেথড দিয়ে index অনুযায়ী আইটেম মুছা হয়, আর কোনো index না দিলে এটি শেষ আইটেম মুছে ফেলে।
+pop_item = my_list.pop(2)
+print(pop_item)
+
+# 7. List Concatenation:
+
+list1 = [1, 2, 3]
+list2 = [4, 5]
+combined_list = list2 + list1
+print(combined_list)
+
+# 8. আইটেমের উপস্থিতি চেক করা (Check Item Presence): in এবং not in অপারেটর ব্যবহার করে চেক করা যায় কোনো আইটেম list-এ আছে কিনা।
+
+my_list = [1, 2, 3, 4]
+check_presence = 8 in my_list
+if check_presence:
+     print(check_presence)
+else:
+    print(check_presence)
+print(5 not in my_list)  # Output: True
+
+
+# 9. List-এর দৈর্ঘ্য (Length of List): len() ফাংশন দিয়ে list-এর দৈর্ঘ্য (কতগুলো আইটেম আছে) জানা যায়।
+
+print(len(my_list))
+
+
+# List Comprehension: List comprehension একটি সংক্ষিপ্ত উপায় যা দিয়ে আমরা একটি list তৈরির জন্য একটি loop বা শর্তাবলীর ব্যবহার করতে পারি।
+# সাধারণ loop দিয়ে
+comprehension_list = []
+
+for i in range(1,6):
+    comprehension_list.append(i**2)
+
+print(comprehension_list)
+
+# List comprehension দিয়ে
+comprehension_list1 = [ i**2 for i in range(1,9)]
+print(comprehension_list1)
+
+
+
+# 4. Item modification (Possible with lists inside tuples): যেহেতু tuple immutable, তাই tuple-এর আইটেম পরিবর্তন করা সম্ভব নয়। তবে যদি tuple-এর মধ্যে কোনো mutable ডেটা টাইপ (যেমন list) থাকে, তাহলে সেই mutable ডেটা পরিবর্তন করা যেতে পারে।
+
+
+my_tuple = (1, 2, [10, 20])
+
+my_tuple[2][1] = 500
+print(my_tuple)
+
+
+# 7. Tuple Unpacking:
+
+my_tuple1 = (10, 20, 30)
+a, b, c = my_tuple1
+
+print(my_tuple1[0])
+
+# Tuple ব্যবহার করার উদাহরণ:
+
+def get_student_info():
+    name = "Alice"
+    age = 22
+    grade = "A"
+    return (name, age, grade)  # Tuple হিসেবে মানগুলো ফেরত দেয়া
+
+info = get_student_info()
+print(info)  # Output: ('Alice', 22, 'A')
+
+# Tuple unpacking
+name, age, grade = info
+print(f"Name: {name}, Age: {age}, Grade: {grade}")
